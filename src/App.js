4b811f9d3d8df026da2244e4251b3ed8b84915e3
Raphael Anizio da Silva <raphaelanizio01@gmail.com>
2023-08-17 14:17:15 -0300

    aprendedo usar o react router dom

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+// vamos instalar o react router dom versao 6 - npm install react-router-dom@6
+// o react router dom vai nos permitir navegar entre as rotas da aplicaçao ou seja entre as paginas através do seu caminho
+
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Inicio from "./paginas/Inicio";
+import SobreMim from "./paginas/SobreMim";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // o BrowserRouter e um componente que vai nos permitir utilizar alguns componentes do react router dom para ter um sistemas de rotas
+    <BrowserRouter>
+
+      {/* o Routes vai ser o nosso roteador ele que vai decidir qual e a rota que vai mostrar na tela  */}
+      <Routes>
+
+        {/* dentro do Route vamos colocar as informaçoes da rota path="/" = caminho, element={} = vai mostrar uma pagina especifica
+        http://localhost:3000/ = pagina Inicio */}
+        <Route path="/" element={<Inicio />} />
+
+        {/* http://localhost:3000/sobremim = pagina SobreMim */}
+        <Route path="/sobremim" element={<SobreMim />} />
+
+        {/* http://localhost:3000/xxxwewf = quando nao conseguir indetificar o caminho da route
+        o * funciona como um servidor coringa do react router dom, essa rota com * ela e selecionando quando o caminho nao e identificado  */}
+        <Route path="*" element={<h2>Página nao encontrada</h2>} />
+
+        
+
+      </Routes>
+    </BrowserRouter>
   );
 }
 
